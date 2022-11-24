d91280aa7f7e950a397e11e35251c36c46caca46
Laura Fontolan <laura_fontolan@hotmail.com>
2022-11-24 13:22:16 -0300

    create header

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,12 @@
+import { Container, Title, Theme, Text } from "./style"
+
+export const Header = () => {
+    return (
+        <Container>
+            <Title>calc</Title>
+            <Theme>
+                <Text>theme</Text>
+            </Theme>
+        </Container>
+    )
+}
---
+++ src/components/Header/style.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 300px;
+    padding: 1rem 0;
+`
+
+export const Title = styled.h1`
+    color: var(--white);
+    font-size: 1.2rem;
+`
+
+export const Theme = styled.div`
+
+`
+
+export const Text = styled.p`
+    color: var(--white);
+    font-size: 0.6rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+`
